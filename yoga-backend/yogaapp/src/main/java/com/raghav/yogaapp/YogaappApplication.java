27f639198ec6f0fbc29bb59b729a2121eca2df02
raghav-pant17 <79854595+raghav-pant17@users.noreply.github.com>
2023-09-21 22:36:11 +0530

    Add files via upload

---
+++ yoga-backend/yogaapp/src/main/java/com/raghav/yogaapp/YogaappApplication.java
@@ -0,0 +1,13 @@
+package com.raghav.yogaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaappApplication.class, args);
+	}
+
+}
